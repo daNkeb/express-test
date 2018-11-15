8e77a80be69321a00f364be6527f35a42acd1789
Sham <lasidow@ya.ru>
2018-11-15 17:17:13 +0300

    hello world

--- dist/myapp.js
+++ dist/myapp.js
@@ -1,5 +1,7 @@
-let express = require('express');
-let app = express();
+'use strict';
+
+var express = require('express');
+var app = express();
 
 app.get('/', function (req, res) {
   res.send('Hello World!');

6a9673d9147650d19902c530a310a453b810aa76
Sham <lasidow@ya.ru>
2018-11-17 15:36:38 +0300

    scripts

--- dist/myapp.js
+++ dist/myapp.js
@@ -3,8 +3,10 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'pug');
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.render('index', { title: 'Hey', message: 'Hello there!' });
 });
 
 app.listen(3000, function () {

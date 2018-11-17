234ad41bca3f108e8d63fe28fb4a8c214d3710f8
Sham <lasidow@ya.ru>
2018-11-17 15:07:02 +0300

    template engine added

--- src/myapp.js
+++ src/myapp.js
@@ -1,9 +1,11 @@
 let express = require('express');
 let app = express();
 
+app.set('view engine', 'pug')
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+  res.render('index', { title: 'Hey', message: 'Hello there!' })
+})
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
